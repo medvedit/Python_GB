0adf3ce8adcd137358520b0498f20c70d750af6e
medvedit <113551136+medvedit@users.noreply.github.com>
2023-01-07 13:39:55 +0300

    add set

---
+++ Python_Function_Collections/set.py
@@ -0,0 +1,9 @@
+#  Ресурс DOCS-PYTHON.RU посвящен языку программирования Python3 и представляет из себя справочную документацию. https://docs-python.ru
+# ======================================================= Методы в set (множество) ===========================================================
+#
+#                             'add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 'intersection',
+#                                 'intersection_update', 'isdisjoint', 'issubset', 'issuperset', 'pop', 'remove',
+#                                   'symmetric_difference', 'symmetric_difference_update', 'union', 'update'
+#
+# =============================================================================================================================================
+#
