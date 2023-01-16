7f2490e625ef6b299f0860a5e10128987199ef01
medvedit <113551136+medvedit@users.noreply.github.com>
2023-01-16 23:46:23 +0300

    Add task 4, 5 and file txt

--- Seminar/Seminar_4_parallel_group.py
+++ Seminar/Seminar_4_parallel_group.py
@@ -27,14 +27,16 @@ min_max = user_output(ls)
 print(f'Min end Max -> {min_max}')
 
 # Задача №2 ______________________________________________________________
-# Найдите корни квадратного уравнения Ax**2 + Bx + C = 0, С помощью модуля. Запросите значения Фб Иб С 3 раза.
+# Найдите корни квадратного уравнения Ax**2 + Bx + C = 0, С помощью модуля. Запросите значения A, B, C 3 раза.
 # Уравнения и корни запишите в файл.
 
 from math import sqrt
 
 def discriminant(a, b, c):
     D = b**2-4*a*c
-    with open('/Users/Medwed_SA/Desktop/Education/Python/Знакомство с языком Python/Lecture_end_Seminare/Python_GB/Seminar/task2_file.txt', 'a', encoding="utf-8") as my_file:
+    print(D)
+    with open('/Users/Medwed_SA/Desktop/Education/Python/Знакомство с языком Python/Lecture_end_Seminare/'\
+                'Python_GB/Seminar/2_Sem_4_task2_file.txt', 'a', encoding="utf-8") as my_file:
         my_file.write(F'{a}x**2 + {b}x + {c} = 0\n')
         if D > 0:
             x_1 = (-b+sqrt(D))/(2*a)
@@ -50,7 +52,7 @@ def discriminant(a, b, c):
 discriminant(3, 12, -12)
 
 # Задача №3 ______________________________________________________________
-# На вход программе подается число N. Программа печатает численны треугольник. Используем вложенные циклы.
+# На вход программе подается число N. Программа печатает числа в треугольник. Используем вложенные циклы.
 
 x = int(input('Введите число: '))
 for i in range(1, x + 1):
@@ -63,10 +65,12 @@ for i in range(1, x + 1):
 # Задача №1: --------------------------------------------------------------------------------------------------------------
 # # Вычислить число с заданной точностью d.
 
+# Вариант 1 ___________________________________________
+
 from decimal import Decimal
 
-number = Decimal(input('Введите число: '))
-precision_number = Decimal(input('Введите точность: '))
+number = Decimal(input('Введите число вещественное число: '))
+precision_number = Decimal(input('Введите точность вывода вещественного числа: '))
 
 number_out = number.quantize(Decimal(precision_number))
 
@@ -75,10 +79,21 @@ print(number_out)
 #        Введите точность: 4949399595.234
 #        1234.923
 
+# Вариант 2 ___________________________________________
+
+num = float(input('Введите число вещественное число: '))
+
+_, accuracy = input('Введите точность вывода введенного ранее вещественного числа числа: ').split(".") #Введенное число через сплит, через разделитель
+#                                                                                  записываем левую часть в безымянную переменную _,
+#                                                                                 а левую в переменную accuracy.
+print(f'{num:.{len(accuracy)}f}') # по длине accuracy делаем вывод переменной num
+
+
+
 # Задача №2: --------------------------------------------------------------------------------------------------------------
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
-n = 99800
+n = 99876700
 
 def prime_factors_number(n:int) -> list:
     out = []
@@ -97,9 +112,8 @@ print(prime_factors_number(n)) # [2, 2, 2, 5, 5, 499]
 # Задайте последовательность чисел. Напишите программу,
 # которая выведет список неповторяющихся элементов последовательности, в том же порядке.
 
-
+#  Вариант 1 : НО COUNT СЪЕДАЕТ МНОГО РЕСУРСОВ!!!___________________
 from random import uniform
-
 
 def random_list(count):
     while count < 0 or count <= 1:
@@ -115,19 +129,100 @@ def non_repeating(val_ls):
             result.append(val_ls[i])
     return result
 
-list_out = random_list(int(input('Введите количество элементов в списке: ')))
+list_out = random_list(int(input('Введите число, количество элементов в списке:')))
 print(list_out)
 print(f'Эти элементы в встречаются один раз -> {non_repeating(list_out)}')
 
-# *Задача №4: --------------------------------------------------------------------------------------------------------------
-# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (от 0 до 10) многочлена, записать в файл полученный
-# многочлен не менее трех раз.
+# Вариант 2: _________________________________________
 
+from random import uniform
+
+def random_list(count: int) -> list:
+    while count < 0 or count <= 1:
+        return 'Вы хотите разобрать стоку? Пожалуйста...'
+    rnd_list = [(int(uniform(1, count))) for i in range(count)]
+    return rnd_list
+
+def search_repetition(n_list: list) -> list:
+    result = []
+    my_dict = {}.fromkeys(n_list, 0) # из входящего списка формируем словарь  с ключами
+    print(my_dict)
+
+    for i in n_list:
+        my_dict[i] += 1
+
+    for k in my_dict:
+        if my_dict[k] ==1:
+            result.append(k)
+
+    return result
+
+list_out = random_list(int(input('Введите число, количество элементов в списке:')))
+print(list_out)
+print(f'Эти элементы в встречаются один раз -> {search_repetition(list_out)}')
+
+# Вариант 3: _________________________________________
+
+from collections import Counter
+
+first_list = [1, 2, 2, 5, 45, 6, 3, 1, 8, 6, 9, 65, 8, 4, 9, 7, 54, 0]
+second_list = Counter(first_list)
+
+result = [x for x, n in second_list.items() if n == 1]
+
+print(result)# [5, 45, 3, 65, 4, 7, 54, 0]
+
+
+
+# *Задача №4: ---------------------------------------------------------------------------------------------
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (от 0 до 10) многочлена,
+# записать в файл полученный многочлен не менее трех раз.
+
+
+from random import randint
+
+k = int(input('Введите натуральную степень k:'))
+
+
+def polynomial_coefficient(degree_in: int):
+    max_val=10
+    # коэфф. при старшей степени не должен быть равен 0
+    degree_in=[randint(0,max_val) for i in range(k)]+[randint(1,max_val)]
+    poly='+'.join([f'{(j,"")[j==1]}x^{i}' for i, j in enumerate(degree_in) if j][::-1])
+    # Поиск и замены:
+    poly=poly.replace('x^1+', 'x+')
+    poly=poly.replace('x^0', '')
+    poly+=('','1')[poly[-1]=='+']
+    poly=(poly, poly[:-2])[poly[-2:]=='^1']
+    # with open('/Users/Medwed_SA/Desktop/Education/Python/Знакомство с языком Python/'\
+    #             'Lecture_end_Seminare/Python_GB/Seminar/3_poly_Sem_4_task_4.txt', 'a', encoding="utf-8") as my_file:
+    #     my_file.write(F'{poly}\n')
+    # with open('/Users/Medwed_SA/Desktop/Education/Python/Знакомство с языком Python/Lecture_end_Seminare/'\
+    #             'Python_GB/Seminar/3.1_poly_Sem_4_task_4.txt', 'a', encoding="utf-8") as my_file:
+    #     my_file.write(F'{poly}\n')
+    return poly
+
+print(polynomial_coefficient(k))
 
 
 # **Задача №5: --------------------------------------------------------------------------------------------------------------
 # Даны два файла, в каждом из которых находится запись многочленов. Задача - сформировать файл, содержащий сумму многочленов.
 
+def polynomial_sum(name_1:str, name_2:str):
+    with open(name_1, "r", encoding="utf-8") as my_f_1, \
+            open(name_2, "r", encoding="utf-8") as my_f_2:
+        first = my_f_1.readline()
+        second = my_f_2.readline()
+
+        if len(first) == len(second):
+            with open('/Users/Medwed_SA/Desktop/Education/Python/Знакомство с языком Python/'\
+                        'Lecture_end_Seminare/Python_GB/Seminar/4_poly_Sem_4_task_5.txt','a', encoding="utf-8") as my_f_3:
+                for i, v in enumerate(first):
+                    my_f_3.write(f'{v[:-5]} + {second[i]}')
+        else:
+            print('Содержимое файлов не совпадает.')
+
+polynomial_sum('3_poly_Sem_4_task_4.txt', '3.1_poly_Sem_4_task_4.txt')
 
 
 
