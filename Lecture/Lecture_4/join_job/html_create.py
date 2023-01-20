63ed8ca78b6a8791d78163bf1455be6ba0d1a896
medvedit <113551136+medvedit@users.noreply.github.com>
2023-01-20 17:03:29 +0300

    end

---
+++ Lecture/Lecture_4/join_job/html_create.py
@@ -0,0 +1,41 @@
+from user_interface import temperature_view
+from user_interface import pressure_view
+from user_interface import wind_speed_view
+
+
+def create(device = 1):
+    style = 'style="font-size:30px;"'
+    html = '<html>\n  <head></head>\n  <body>\n'
+    html += '    <p {}>Temperature: {} c</p>\n'\
+        .format(style, temperature_view(device))
+    html += '    <p {}>Wind_speed: {} m/s</p>\n'\
+        .format(style, wind_speed_view(device))
+    html += '    <p {}>Pressure: {} mmHg</p>\n'\
+        .format(style, pressure_view(device))
+    html += '  </body>\n</html>'
+
+    with open('/Users/Medwed_SA/Desktop/Education/Python/'\
+            'Знакомство с языком Python/Lecture_end_Seminare/'\
+            'Python_GB/Lecture/Lecture_4/join_job/index.html', 'w') as page:
+        page.write(html)
+
+    return html
+
+
+
+# def new_create(data , device = 1):
+#     t, p, w = data
+#     style = 'style="font-size:30px;"'
+#     html = '<html>\n  <head></head>\n  <body>\n'
+#     html += '    <p {}>Temperature: {} c</p>\n'\
+#         .format(style, t)
+#     html += '    <p {}>Wind_speed: {} m/s</p>\n'\
+#         .format(style, w)
+#     html += '    <p {}>Pressure: {} mmHg</p>\n'\
+#         .format(style, p)
+#     html += '  </body>\n</html>'
+
+#     with open('new_index.html', 'w') as page:
+#         page.write(html)
+
+#     return data
