b3d535fc95bafd282b2e3f3f8b4d1ddea795a4f0
medvedit <113551136+medvedit@users.noreply.github.com>
2023-01-25 11:50:22 +0300

    было несколько тестов и что то обнавилось))))

--- Seminar/Seminar_7/partobusi/clear.py
+++
@@ -1,9 +0,0 @@
-import os  # для с операционными системами.
-from sys import platform  # информация об операционной системе.
-
-
-def clear_screen():  # Очистка консоли
-    if platform == "linux" or platform == "linux2" or platform == "darwin":
-        os.system("clear")  # для Linux & MacOS
-    elif platform == "win32" or platform == "cygwin":
-        os.system("cls")    # для Windows
