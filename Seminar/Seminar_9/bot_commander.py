b887e08c1f09929dbcc520fe33d7ef064438f5a6
medvedit <113551136+medvedit@users.noreply.github.com>
2023-01-31 21:02:08 +0300

    Закончил писать bot_commands

--- Seminar/Seminar_9/bot_commander.py
+++ Seminar/Seminar_9/bot_commander.py
@@ -12,12 +12,12 @@ async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Здравствуй {update.effective_user.first_name} ' + emoji.emojize(':thumbs_up:'))
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'/hi => Тут я здороваюсь ' + emoji.emojize(':cowboy_hat_face:\n'))
+    await update.message.reply_text(f'/hi => Тут я здороваюсь ' + emoji.emojize(':cowboy_hat_face:\n') +
+                                    '/call => А тут я посчитаю')
 
 async def calculator_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    x = update.message.text(update.chat.id, 'привет, введи любой текст и отправь')
-
-    # x = int(input('Введите первое число: '))
-    y = int(input('Введите второе число: '))
-
+    msg = update.message.text
+    items = msg.split()
+    x, y = int(items[1]), int(items[2])
+    # y = int(items[2])
     await update.message.reply_text(f'{x} + {y} = {x+y}')
