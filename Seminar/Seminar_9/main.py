75abc5855438f084acb1a5efd90f5794377a7fae
medvedit <113551136+medvedit@users.noreply.github.com>
2023-01-31 21:03:34 +0300

    Исправил имя команды вызова

--- Seminar/Seminar_9/main.py
+++ Seminar/Seminar_9/main.py
@@ -3,13 +3,14 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commander import *
 import emoji
 
+
 app = ApplicationBuilder().token("6089710413:AAEUiTHzXCYM-O03trnXs3g3nE4CEkFZnI0").build()
 
 
 app.add_handler(CommandHandler("start", start_command))
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("help", help_command))
-app.add_handler(CommandHandler("cal", calculator_command))
+app.add_handler(CommandHandler("call", calculator_command))
 
 print(f'Сервер запущен ' + emoji.emojize(':dizzy:'))
 
