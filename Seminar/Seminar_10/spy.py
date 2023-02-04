8eebba43fa8f74a851e8bca53ab29ba410f7a3ed
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-04 15:37:30 +0300

    Добавил лог дата и время

--- Seminar/Seminar_10/spy.py
+++ Seminar/Seminar_10/spy.py
@@ -1,7 +1,9 @@
 from telegram import Update
 from telegram.ext import CallbackContext
+import datetime
+
 
 def log(update: Update, context: CallbackContext):
     file = open('db.csv', 'a')
-    file.write(f'{update.effective_user.first_name},{update.effective_user.id}, {update.message.text}\n')
+    file.write(f'{update.effective_user.first_name},{update.effective_user.id}, {update.message.text}, {datetime.datetime.today()}\n')
     file.close()
